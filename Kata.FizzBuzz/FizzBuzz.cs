1e49ebe7cc1b2c9c6616d9800de1326efb0f3bc9
enrico <enricozob@gmail.com>
2022-06-18 22:07:52 +0200

    Add test and implementations for fizz and buzz methos

--- Kata.FizzBuzz/FizzBuzz.cs
+++ Kata.FizzBuzz/FizzBuzz.cs
@@ -1,7 +1,26 @@
 ﻿namespace Kata.FizzBuzz
 {
-    public class Class1
+    public static class FizzBuzz
     {
+        public static string PrintNumber()
+        {
+            var number = string.Empty;
+            for (int i = 0; i < 100; i++)
+                number += IsFizz(i) ? " " + "Fizz" : (IsBuzz(i) ?  " " + "Buzz" : " " + i);
+                
 
+            return number.Trim();
+        }
+
+        public static bool IsFizz(int n)
+        {
+            return n % 3 == 0;
+        }
+
+        public static bool IsBuzz(int n)
+        {
+            return n % 5 == 0;
+        }
+        
     }
 }
--- Kata.FizzBuzz/FizzBuzzTest.cs
+++ Kata.FizzBuzz/FizzBuzzTest.cs
@@ -3,10 +3,24 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Xunit;
 
 namespace Kata.FizzBuzz
 {
-    internal class FizzBuzzTest
+    public class FizzBuzzTest
     {
+        [Fact]
+        public void CanReturnFizzIfNumberIsDivisibleByThree()
+        {
+            var actualResult = FizzBuzz.PrintNumber();
+            Assert.Contains("Fizz", actualResult);
+        }
+
+        [Fact]
+        public void CanReturnBuzzIfNumberIsDivisibleByFive()
+        {
+            var actualResult = FizzBuzz.PrintNumber();
+            Assert.Contains("Buzz", actualResult);
+        }
     }
 }
