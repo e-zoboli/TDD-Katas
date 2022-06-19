2ccbb4ad3a8f57993b3b130bf669bb391beef6f4
enrico <enricozob@gmail.com>
2022-06-19 15:16:59 +0200

    Modify search method with binary search

--- Kata.KarateChop/KarateChop.cs
+++ Kata.KarateChop/KarateChop.cs
@@ -8,9 +8,40 @@ namespace Kata.KarateChop
 {
     public static class KarateChop
     {
-        public static int Search(int n, int[] arr)
+        public static int Search(int n, int[] numbers)
         {
+            //Simple iteration
+            //int index = 0;
+            //for(int i = 0; i < arr.Length; i++)
+            //{
+            //    if (arr[i] == n)
+            //    {
+            //        index = i;
+            //        return index;
+            //    }
+            //}
+            //return -1;
+
+            //IndexOf method
+            //return Array.IndexOf(arr, n);
+
+            // Binary Search
+            int min = 0;
+            int max = numbers.Length - 1;
+            while(min <= max)
+            {
+                int middle = (min + max) / 2;
+                if (numbers[middle] == n)
+                    return middle;
+                else if (numbers[middle] > n)
+                    max = middle - 1;
+                else 
+                    min = middle + 1;             
+            }
             return -1;
+            
+
+
         }
     }
 }
--- Kata.KarateChop/KarateChopTest.cs
+++ Kata.KarateChop/KarateChopTest.cs
@@ -5,10 +5,38 @@ namespace Kata.KarateChop
     public class KarateChopTest
     {
         [Fact]
-        public void AnEmptyArrayShouldReturnMinusOne()
+        public void SearchOnAnEmptyArrayShouldReturnMinusOne()
         {
-            var testArr = new int[] { };
-            Assert.Equal(-1, KarateChop.Search(3, testArr));
+            var testList = new int[] { };
+            Assert.Equal(-1, KarateChop.Search(3, testList));
+
+        }
+        [Fact]
+        public void ShouldReturnMinusOneIfNumberNotInArray()
+        {
+            var testList = new int[] { 1 };
+            Assert.Equal(-1, KarateChop.Search(3, testList));
+
+        }
+        [Fact]
+        public void ShouldReturnZeroIfNumberIsInArrayAtPositionZero()
+        {
+            var testList = new int[] { 3 };
+            Assert.Equal(0, KarateChop.Search(3, testList));
+
+        }
+        [Fact]
+        public void ShouldReturnCorrectIndexIfNumberIsInArray()
+        {
+            var testList = new int[] { 2, 3, 5 };
+            Assert.Equal(1, KarateChop.Search(3, testList));
+
+        }
+        [Fact]
+        public void ShouldReturnCorrectIndexIfNumberIsInLongerArray()
+        {
+            var testList = new int[] { 2, 5, 6, 10, 16, 99, 134  };
+            Assert.Equal(4, KarateChop.Search(16, testList));
 
         }
     }
